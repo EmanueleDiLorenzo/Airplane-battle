85f15580684190e6179589965e9021ae58c97439
Babu <dranzen97@hotmail.com>
2015-10-02 11:24:22 +0200

    Implements the background

--- main.lua
+++ main.lua
@@ -2,7 +2,7 @@ debug = true
 
 require("scene")
 function love.load(arg)
-  background = love.graphics.newImage("assets/sfondo.jpg")
+  background = love.graphics.newImage("assets/stars.gif")
   music = love.audio.newSource("assets/Soundtrack.mp3")
   Gamestate.current_scene = Menu:new()
   Gamestate.current_scene:load(arg)
@@ -14,11 +14,10 @@ function love.update(dt)
 end
 
 function love.draw(dt)
+  for i = 0, love.graphics.getWidth() / background:getWidth() do
+      for j = 0, love.graphics.getHeight() / background:getHeight() do
+           love.graphics.draw(background, i * background:getWidth(), j * background:getHeight())
+       end
+  end
   Gamestate.current_scene:draw(dt)
-  --for i = 0, love.graphics.getWidth() / background:getWidth() do
-      -- for j = 0, love.graphics.getHeight() / background:getHeight() do
-           --love.graphics.draw(background, i * background:getWidth(), j * background:getHeight())
-      -- end
-  -- end
-
 end
